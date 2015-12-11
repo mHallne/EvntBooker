e67c0b556825c87e4ff9f37fa5008ab5eb904278
Mikael Hallne <mikael.hallne@gmail.com>
2015-12-11 21:30:55 +0100

    Inital commit

---
+++ EventBooker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventBooker.MvcApplication" Language="C#" %>
---
+++ EventBooker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventBooker.Startup))]
+namespace EventBooker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
